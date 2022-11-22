014771562c7409a445f876dcb68f7cb50677794d
Tony Duong <tony.duong.102@gmail.com>
2022-11-22 22:23:26 +0900

    :sparkles: finalizing 'restaurant' screen

--- components/DishRow.js
+++ components/DishRow.js
@@ -3,10 +3,23 @@ import React, { useState } from 'react'
 import Currency from "react-currency-formatter"
 import { urlFor } from '../sanity'
 import { MinusCircleIcon, PlusCircleIcon } from 'react-native-heroicons/solid'
+import { useDispatch, useSelector } from 'react-redux'
+import { addToBasket, selectBasketItemsWithId, removeFromBasket } from '../slices/basketSlice'
 
 const DishRow = ({ id, name, description, price, image}) => {
   const [isPressed, setIsPressed] = useState(false)
-  const [items, setItems] = useState([])
+  const items = useSelector((state) => selectBasketItemsWithId(state, id))
+  const dispatch = useDispatch()
+
+  const addItemToBasket = () => {
+    dispatch(addToBasket({ id, name, description, price, image }))
+  }
+
+  const removeItemFromBasket = () => {
+    if (!items.length > 0) return;
+
+    dispatch(removeFromBasket({ id }))
+  }
 
   return (
     <>
@@ -37,18 +50,18 @@ const DishRow = ({ id, name, description, price, image}) => {
         isPressed && (
           <View className="bg-white px-4">
             <View className="flex-row items-center space-x-2 pb-3">
-              <TouchableOpacity>
+              <TouchableOpacity onPress={removeItemFromBasket}>
                 <MinusCircleIcon
                   color={items.length > 0 ? "#00CCBB" : "gray"}
                   size={40}
                 />
               </TouchableOpacity>
 
-              <Text>0</Text>
+              <Text>{ items.length }</Text>
 
-              <TouchableOpacity>
+              <TouchableOpacity onPress={addItemToBasket}>
                 <PlusCircleIcon
-                  color={items.length > 0 ? "#00CCBB" : "gray"}
+                  color="#00CCBB"
                   size={40}
                 />
               </TouchableOpacity>
